2567d19e44fcd872d5a3939f2f0b79a895af2017
klajdi <shehaj.klajdi@outlook.com>
2023-08-29 10:17:52 +0200

    livechat ui frontend

---
+++ Block/Chat.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Leeto\TicketLiveChat\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class Chat extends Template
+{
+}
